60c62bc7b92b922ba519264034674549e33755c0
Mirlan2121 <zhumataevmirlan@gmail.com>
2021-10-07 15:14:42 +0600

    Homework

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,30 @@
+public class Calculator {
+
+    public static int calculate(int a, int b, String operation) {
+        if(operationValidate(operation)) {
+            if(divideByZero(b, operation)) {
+                return switch (operation) {
+                    case "+" -> a + b;
+                    case "-" -> a - b;
+                    case "*" -> a * b;
+                    case "/" -> a / b;
+                    default -> 0;
+                };
+            }
+        }
+        return 0;
+    }
+
+    public static boolean operationValidate(String operation) {
+        return operation.equals("+") ||
+                operation.equals("-") ||
+                operation.equals("*") ||
+                operation.equals("/");
+    }
+
+    public static boolean divideByZero(int b, String operation) {
+        if(operation.equals("/") && b == 0) return false;
+        return true;
+    }
+}
+
